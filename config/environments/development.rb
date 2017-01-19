57b55508f2b332f62a8280720558e5a55e059333
Pramodh Mazumdar <satyapramodh@gmail.com>
2017-01-19 16:45:13 -0700

    [fix] rails server crashes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,5 +50,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # FIXME rails s crashes with this on bash on ubuntu
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
