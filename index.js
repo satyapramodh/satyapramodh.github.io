0e7778760dc91e71bd76b4b2df52d9a6fd35c580
Pramodh Mazumdar <satyapramodh@gmail.com>
2018-02-13 05:32:05 -0700

    split site into multiple pages

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-$('#myScrollspy').on('activate.bs.scrollspy', function () {
-  // do something…
-  if ($("nav li > a.active").text() != "Intro"){
-    $("nav").css({
-      "display": "flex",
-      "background-color": "white"
-    });
-  } else{
-    $("nav").css({
-      "display": "none",
-      "background-color": "transparent"
-    });
-  }
-})
+// $('#myScrollspy').on('activate.bs.scrollspy', function () {
+//   // do something…
+//   if ($("nav li > a.active").text() != "Intro"){
+//     $("nav").css({
+//       "display": "flex",
+//       "background-color": "white"
+//     });
+//   } else{
+//     $("nav").css({
+//       "display": "none",
+//       "background-color": "transparent"
+//     });
+//   }
+// })
